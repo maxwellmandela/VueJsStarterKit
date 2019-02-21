e647282b015a2427a6abf026374dd4ab32eccc05
Maxwell Mandela <maxywhyner94@gmail.com>
2019-02-21 16:33:16 +0300

    Updated tests

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -1,10 +1,18 @@
 import { mount } from "@vue/test-utils";
 
 import HelloWorld from "@/components/HelloWorld.vue";
+import App from "@/App.vue";
 
 describe("HelloWorld.vue component", () => {
-  it("should display Essential Links", () => {
+  it("should be called HelloWorld", () => {
     const wrapper = mount(HelloWorld);
     expect(wrapper.name()).toBe("HelloWorld");
+  });
+});
+
+describe("App.vue", () => {
+  it("should be called App", () => {
+    const wrapper = mount(App);
+    expect(wrapper.name()).toBe("App");
   });
 });
