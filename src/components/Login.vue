2f8a37d354eb7834f9d70e1b7ca5b2d076985c3f
Maxwell Mandela <maxywhyner94@gmail.com>
2019-03-14 15:20:18 +0300

    More tests

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -2,6 +2,7 @@ import { mount } from "@vue/test-utils";
 
 import HelloWorld from "@/components/HelloWorld.vue";
 import App from "@/App.vue";
+import Login from "@/components/Login";
 
 describe("HelloWorld.vue component", () => {
   it("should be called HelloWorld", () => {
@@ -14,5 +15,18 @@ describe("App.vue", () => {
   it("should be called App", () => {
     const wrapper = mount(App);
     expect(wrapper.name()).toBe("App");
+  });
+});
+
+describe("Login.vue", () => {
+  it("should be called Login", () => {
+    const wrapper = mount(Login);
+    expect(wrapper.name()).toBe("Login");
+  });
+
+  it("should have two input fields", () => {
+    const wrapper = mount(Login);
+    const expectedForm = { username: "", password: "" };
+    expect(wrapper.vm.form).toMatchObject(expectedForm);
   });
 });
